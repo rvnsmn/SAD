2d9509ef878d23de1a3ab0dc6b36c94f3e40e69a
Raven <ravenledda69@gmail.com>
2024-01-01 20:20:03 +0800

    css link to dashboard

--- kabarangay.php
+++ kabarangay.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1,0">
     <title>Ka-Barangay</title>
+    <link rel="stylesheet" type="text/css" href="css/dashboard.css">
     <link rel="stylesheet" type="text/css" href="css/kabarangay.css">
    </head>
      <div class="menuToggle"></div>
