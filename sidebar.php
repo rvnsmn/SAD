a7fd089b4f3a1c44fad4ba94689eae5933bf270e
Raven <ravenledda69@gmail.com>
2024-01-01 18:13:01 +0800

    Second Commit

--- sidebar.php
+++ sidebar.php
@@ -23,31 +23,31 @@
                                 </a>
                             </li>
                             <li style="--bg: #0fc70f;"<?php echo $PAGE == "kabarangay" ? "class='active'" : ""; ?>>
-                                <a href="../kabarangay.php">
+                                <a href="kabarangay.php">
                                     <div class="icon"><ion-icon name="people"></ion-icon></div>
                                     <div class="text">KA-BARANGAY</div>
                                 </a>
                             </li>
-                            <li style="--bg: #2196f3;">
-                                <a href="../blotter.php"<?php echo $PAGE == "blotter" ? "class='active'" : ""; ?>>
+                            <li style="--bg: #2196f3;"<?php echo $PAGE == "blotter" ? "class='active'" : ""; ?>>
+                                <a href="blotter.php">
                                     <div class="icon"><ion-icon name="newspaper"></ion-icon></div>
                                     <div class="text">BLOTTER</div>
                                 </a>
                             </li>
                             <li style="--bg: #e9d045;"<?php echo $PAGE == "establishment" ? "class='active'" : ""; ?>>
-                                <a href="#">
+                                <a href="establishment.php">
                                     <div class="icon"><ion-icon name="business"></ion-icon></div>
                                     <div class="text">ESTABLISHMENT</div>
                                 </a>
                             </li>
                             <li style="--bg: #e91e63;"<?php echo $PAGE == "covid-19" ? "class='active'" : ""; ?>>
-                                <a href="#">
+                                <a href="covid19.php">
                                     <div class="icon"><ion-icon name="fitness"></ion-icon></div>
                                     <div class="text">COVID-19</div>
                                 </a>
                             </li>
                             <li style="--bg: rgb(30, 233, 182);"<?php echo $PAGE == "certificate" ? "class='active'" : ""; ?>>
-                                <a href="#">
+                                <a href="certificate.php">
                                     <div class="icon"><ion-icon name="receipt"></ion-icon></div>
                                     <div class="text">CERTIFICATE</div>
                                 </a>
@@ -58,7 +58,7 @@
                     <div class="bottom">
                         <ul>
                             <li style="--bg: #333;">
-                                <a href="#">
+                                <a href="dashboard.php">
                                     <div class="icon">
                                     <div class="imgBx">
                                         <img src="img/376522276_1023314158721897_2179476028798761998_n.jpg">
